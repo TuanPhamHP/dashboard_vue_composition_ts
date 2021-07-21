118460821cd44bb8bb796cc3ca91aca63ecf0d8a
TuanPhamHP <phamanhtuan290896@gmail.com>
2021-07-21 14:34:08 +0700

    method trigger logout modal

--- src/components/popup/ConfirmSignout.vue
+++ src/components/popup/ConfirmSignout.vue
@@ -1,32 +1,35 @@
 <template>
-  <div class="sign-out">
-    <div class="header">Do you really want to Sign out?</div>
-    <div class="buttons">
-      <div class="cancel">Cancel</div>
-      <div class="yes" @click="signOut">Yes</div>
-    </div>
+ <div class="sign-out">
+  <div class="header">Do you really want to Sign out?</div>
+  <div class="buttons">
+   <div class="cancel" @click="cancel">Cancel</div>
+   <div class="yes" @click="signOut">Yes</div>
   </div>
+ </div>
 </template>
 
 <script lang="ts">
-import { reactive, ref, defineComponent } from "@vue/composition-api";
-import store from "@/store";
-export default defineComponent({
+ import { reactive, ref, defineComponent } from "@vue/composition-api";
+ import store from "@/store";
+ export default defineComponent({
   setup: () => {
-    return {};
+   return {};
   },
   watch: {},
   methods: {
-    signOut() {
-      localStorage.removeItem("auth._token.local");
-      store.commit("SET_LOGOUT_USER");
-      this.$router.push("/login");
-    },
+   signOut() {
+    localStorage.removeItem("auth._token.local");
+    store.commit("SET_LOGOUT_USER");
+    this.$router.push("/login");
+   },
+   cancel() {
+    this.$store.commit("CONFIRM_LOGOUT", false);
+   },
   },
-});
+ });
 </script>
 <style lang="scss" scoped>
-.sign-out {
+ .sign-out {
   position: absolute;
   top: 30%;
   left: 800px;
@@ -37,24 +40,24 @@ export default defineComponent({
   z-index: 14;
   border-radius: 4px;
   .header {
-    text-align: center;
+   text-align: center;
   }
   .buttons {
-    display: flex;
-    justify-content: space-around;
-    margin: 24px 28px;
-    .cancel {
-      border: 1px solid #202224;
-      padding: 0 8px;
-      text-transform: uppercase;
-      cursor: pointer;
-    }
-    .yes {
-      border: 1px solid #202224;
-      padding: 0 8px;
-      text-transform: uppercase;
-      cursor: pointer;
-    }
+   display: flex;
+   justify-content: space-around;
+   margin: 24px 28px;
+   .cancel {
+    border: 1px solid #202224;
+    padding: 0 8px;
+    text-transform: uppercase;
+    cursor: pointer;
+   }
+   .yes {
+    border: 1px solid #202224;
+    padding: 0 8px;
+    text-transform: uppercase;
+    cursor: pointer;
+   }
   }
-}
+ }
 </style>
