7f7dc51e8fa3b5247197da8ce0baadd6071257ac
TrungHN <doubleblade98@gmail.com>
2021-07-22 16:35:01 +0700

    rename

--- src/router/index.ts
+++ src/router/index.ts
@@ -50,7 +50,7 @@ const routes: Array<RouteConfig> = [
    },
  {
   path: "/manifest",
-  name: "manifest",
+  name: "Manifest",
   // route level code-splitting
   // this generates a separate chunk (about.[hash].js) for this route
   // which is lazy-loaded when the route is visited.
