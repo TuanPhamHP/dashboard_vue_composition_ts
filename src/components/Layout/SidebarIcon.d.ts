6ea72d652e02f89d6618843f83f0559c41bc5e2d
TuanPhamHP <phamanhtuan290896@gmail.com>
2021-07-23 14:36:22 +0700

    update sidebar icon .d.ts

---
+++ src/components/Layout/SidebarIcon.d.ts
@@ -0,0 +1,16 @@
+declare module "*.svg" {
+ const value: string;
+ export const value;
+}
+declare module "*.png" {
+ const value: string;
+ export const value;
+}
+declare module "*.jpg" {
+ const value: string;
+ export const value;
+}
+declare module "*.jpeg" {
+ const value: string;
+ export const value;
+}
