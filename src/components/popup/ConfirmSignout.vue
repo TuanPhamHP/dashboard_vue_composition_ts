327189789912c885b19aa2e04abeaae9c188d491
TuanPhamHP <phamanhtuan290896@gmail.com>
2021-07-23 11:03:20 +0700

    update color variables

--- src/components/popup/ConfirmSignout.vue
+++ src/components/popup/ConfirmSignout.vue
@@ -1,62 +1,60 @@
 <template>
-  <v-dialog v-model="logoutIsOpen" persistent max-width="500px">
-    <v-card>
-      <v-card-title class="header">
-        Do you really want to Sign out?
-      </v-card-title>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text @click="cancel" class="cancel"> Cancel </v-btn>
-        <v-btn text @click="signOut" class="yes"> Yes </v-btn>
-      </v-card-actions>
-      <!-- <div class="sign-out">
+ <v-dialog v-model="logoutIsOpen" persistent max-width="500px">
+  <v-card>
+   <v-card-title class="header py-5"> <p class="mb-0 py-5">Do you really want to Sign out?</p> </v-card-title>
+   <v-card-actions>
+    <v-spacer></v-spacer>
+    <v-btn text @click="cancel" class="cancel"> Cancel </v-btn>
+    <v-btn text @click="signOut" class="yes"> Yes </v-btn>
+   </v-card-actions>
+   <!-- <div class="sign-out">
         <div class="header">Do you really want to Sign out?</div>
         <div class="buttons">
           <div class="cancel" @click="cancel">Cancel</div>
           <div class="yes" @click="signOut">Yes</div>
         </div>
       </div> -->
-    </v-card>
-  </v-dialog>
+  </v-card>
+ </v-dialog>
 </template>
 
 <script lang="ts">
-import { reactive, ref, defineComponent } from "@vue/composition-api";
-import { mapState } from "vuex";
-import store from "@/store";
-export default defineComponent({
+ import { reactive, ref, defineComponent } from "@vue/composition-api";
+ import { mapState } from "vuex";
+ import store from "@/store";
+ export default defineComponent({
   setup: () => {
-    return {};
+   return {};
   },
   watch: {},
   computed: {
-    ...mapState({
-      logoutIsOpen: (state: any): boolean => state.logoutIsOpen,
-    }),
+   ...mapState({
+    logoutIsOpen: (state: any): boolean => state.logoutIsOpen,
+   }),
   },
   methods: {
-    signOut() {
-      localStorage.removeItem("auth._token.local");
-      store.commit("SET_LOGOUT_USER");
-      this.$router.push("/login");
-      this.$store.commit("CONFIRM_LOGOUT", false);
-    },
-    cancel() {
-      this.$store.commit("CONFIRM_LOGOUT", false);
-    },
+   signOut() {
+    localStorage.removeItem("auth._token.local");
+    store.commit("SET_LOGOUT_USER");
+    this.$router.push("/login");
+    this.$store.commit("CONFIRM_LOGOUT", false);
+   },
+   cancel() {
+    this.$store.commit("CONFIRM_LOGOUT", false);
+   },
   },
-});
+ });
 </script>
 <style lang="scss" scoped>
-.header {
+ .header {
   text-align: center;
   font-weight: bold;
   font-size: 20px;
   line-height: 37px;
   letter-spacing: -0.114286px;
   color: #202224;
-}
-.cancel {
+ }
+ .cancel {
   border: 2px solid #4880ff;
   border-radius: 12px;
   padding: 0 8px;
@@ -71,8 +69,8 @@ export default defineComponent({
   color: #4880ff;
   text-transform: uppercase;
   cursor: pointer;
-}
-.yes {
+ }
+ .yes {
   border: 1px solid #4880ff;
   padding: 0 8px;
   height: 36px;
@@ -91,5 +89,5 @@ export default defineComponent({
   letter-spacing: -0.0642857px;
 
   color: #ffffff;
-}
+ }
 </style>
