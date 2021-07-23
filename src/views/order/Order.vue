979359c1064517d3c3c8f24a511905de0bc63064
TrungHN <doubleblade98@gmail.com>
2021-07-23 14:58:37 +0700

    order

---
+++ src/views/order/Order.vue
@@ -0,0 +1,31 @@
+<template>
+ <DefaultLayout>
+  <div class="px-3">
+   <ContainerOrder />
+  </div>
+ </DefaultLayout>
+</template>
+<script lang="ts">
+ import { defineComponent  } from "@vue/composition-api";
+ import DefaultLayout from "@/layouts/default.vue"; 
+ import ContainerOrder from "@/components/containers/ContainerOrder.vue";
+ export default defineComponent({
+    components: {
+    DefaultLayout,
+    ContainerOrder,
+    },
+    
+    methods:{
+       
+    },
+    created(){
+      this.$store.commit("SET_BREADCRUMB", {
+         viewTxt: "Order",
+         ableToBack: false,
+         rootRouter: "/",
+         hasStatus: false,
+         statusTxt: "",
+      });
+    },
+ });
+</script>
